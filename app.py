3ca02850cf88aaaa4f34ca5a13d34e0186adfcbd
SarahA45 <59462868+SarahA45@users.noreply.github.com>
2020-11-13 19:42:04 +0300

    complete Game's CRUD

--- app.py
+++ app.py
@@ -6,16 +6,14 @@ from flask_cors import CORS
 import sys
 from database.models import setup_db, Game
 from auth.auth import AuthError, requires_auth
+from datetime import datetime
+
 
 app = Flask(__name__)
 setup_db(app)
 CORS(app)
 
-"""
-@(Done) uncomment the following line to initialize the datbase
-!! NOTE THIS WILL DROP ALL RECORDS AND START YOUR DB FROM SCRATCH
-!! NOTE THIS MUST BE UNCOMMENTED ON FIRST RUN
-"""
+
 # db_drop_and_create_all()
 
 
@@ -23,10 +21,10 @@ CORS(app)
 
 # GET all games
 @app.route("/games", methods=["GET"])
-def get_drinks():
+def get_games():
     games = Game.query.all()
-    # if len(games) == 0:
-    #     abort(404)
+    if len(games) == 0:
+        abort(404)
     games = [game.format() for game in games]
 
     return jsonify({"success": True, "games": games}), 200
@@ -35,31 +33,78 @@ def get_drinks():
 # -----------------------------------------------------------------
 
 # POST a new game
+@app.route("/games", methods=['POST'])
+def addGame():
+    body = request.get_json()
+    title = body.get("title", None)
+    about = body.get("about", None)
+    imgSrc = body.get("imgSrc", None)
+    release_year = body.get("releaseYear", None)
+    genres = body.get("genres", None)
+    platforms = body.get("platforms", None)
+    print(about)
+    newGame = Game(title=title, about=about, imgSrc=imgSrc, release_year=release_year,
+                   genres=genres, platforms=platforms, created_at=datetime.now(), updated_at=datetime.now())
 
+    try:
+
+        newGame.insert()
+    except:
+        newGame.undoInsert()
+    return jsonify({"success": True, "new_game": newGame.format()}), 200
 
 
 # -----------------------------------------------------------------
 
 # PATCH game
+@app.route("/games/<game_id>", methods=['PATCH'])
+def updateGameInfo(game_id):
+    game = Game.query.filter(
+        Game.id == game_id).one_or_none()
 
+    if game is None:
+        abort(404)
 
+    body = request.get_json()
+    game.title = body.get("title", None)
+    game.about = body.get("about", None)
+    game.imgSrc = body.get("imgSrc", None)
+    game.release_year = body.get("releaseYear", None)
+    game.genres = body.get("genres", None)
+    game.platforms = body.get("platforms", None)
+    game.updated_at = datetime.now()
+
+    try:
+        game.update()
+    except:
+        game.undoInsert()
+    return jsonify({"success": True, "updated_game": game.format()}), 200
 
 # -----------------------------------------------------------------
 
 # DELETE game
 
 
+@app.route("/games/<game_id>", methods=["DELETE"])
+def deleteGame(game_id):
+    game = Game.query.filter(
+        Game.id == game_id).one_or_none()
+
+    if game is None:
+        abort(404)
+
+    game.delete()
+    return jsonify({"success": True, "deleted": game_id}), 200
 
 # -----------------------------------------------------------------
 
-# POST a new user
-
+# add a new user
+# def addUser(user_email):
 
 
 # -----------------------------------------------------------------
 
 # GET User's records
-
 
 
 # -----------------------------------------------------------------
@@ -72,11 +117,9 @@ def get_drinks():
 # PATCH user's game record (change game's status/ list)
 
 
-
 # -----------------------------------------------------------------
 
 # DELETE a user's game record
-
 
 
 # -----------------------------------------------------------------
@@ -92,7 +135,8 @@ def unprocessable(error):
 @app.errorhandler(404)
 def not_found(error):
     return (
-        jsonify({"success": False, "error": 404, "message": "resource not found"}),
+        jsonify({"success": False, "error": 404,
+                 "message": "resource not found"}),
         404,
     )
 
@@ -108,7 +152,8 @@ def bad_request(error):
 @app.errorhandler(500)
 def internal_server_error(error):
     return (
-        jsonify({"success": False, "error": 500, "message": "internal server error"}),
+        jsonify({"success": False, "error": 500,
+                 "message": "internal server error"}),
         500,
     )
 
@@ -120,10 +165,11 @@ def unauthorized(error):
         401,
     )
 
+
 @app.errorhandler(AuthError)
 def handle_auth_errors(error):
     return (
-        jsonify({"success": False, "error": error.status_code, "message": error.error}),
+        jsonify({"success": False, "error": error.status_code,
+                 "message": error.error}),
         401,
     )
-
--- database/models.py
+++ database/models.py
@@ -1,7 +1,11 @@
 import os
-from sqlalchemy import Column, String, Integer, create_engine
+from sqlalchemy import Column, String, Integer, create_engine, DateTime, ARRAY
 from flask_sqlalchemy import SQLAlchemy
 import json
+from flask_migrate import Migrate
+from datetime import datetime
+from sqlalchemy.sql import func
+
 
 database_name = "caps"
 database_path = "postgres://{}/{}".format("localhost:5432", database_name)
@@ -19,7 +23,8 @@ def setup_db(app, database_path=database_path):
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
     db.init_app(app)
-    db.create_all()
+    # db.create_all()
+    migrate = Migrate(app, db)
 
 
 """
@@ -32,14 +37,30 @@ class Game(db.Model):
 
     id = Column(Integer, primary_key=True)
     title = Column(String)
+    about = Column(String)
+    imgSrc = Column(String)
     release_year = Column(String)
+    genres = Column(ARRAY(String))
+    platforms = Column(ARRAY(String))
+    created_at = Column(DateTime, default=datetime.now())
+    updated_at=Column(DateTime, default=func.now())
 
-    def __init__(self, title, release_year):
+    def __init__(self, title, about, imgSrc, release_year, genres, platforms, created_at, updated_at):
         self.title = title
+        self.about = about
+        self.imgSrc = imgSrc
         self.release_year = release_year
+        self.genres = genres
+        self.platforms = platforms
+        self.created_at = created_at
+        self.updated_at=updated_at
+
     def insert(self):
         db.session.add(self)
         db.session.commit()
+        
+    def undoInsert(self):
+        db.rollback()
 
     def update(self):
         db.session.commit()
@@ -52,13 +73,19 @@ class Game(db.Model):
         return {
             "id": self.id,
             "title": self.title,
+            "about": self.about,
+            "imgSrc": self.imgSrc,
             "release_year": self.release_year,
+            "genres": self.genres,
+            "platforms": self.platforms,
+            "created_at":self.created_at,
+            "updated_at":self.updated_at
         }
+
 
 """
 Game Record
 """
-
 
 
 """
